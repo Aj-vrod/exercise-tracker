9dc1c32efc9a3e89a5a65759ae7ef14f0f544cc5
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-03 21:05:06 +0200

    wrote body-parser setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
