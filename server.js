d9c90b05ae11b384ae2dca38d7d49f65496dd273
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-03 21:06:05 +0200

    exercise schema defined

--- server.js
+++ server.js
@@ -6,12 +6,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.connect(process.env.MONGO_KEY);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+// CREATE SCHEMAS
+const exerciseSchema = new Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: String
+  }
+});
+
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
